629f70162409777e4f25bfb9e14ea48d907f3ff5
Simon Angerbauer <simon.angerbauer@gmx.at>
2015-08-03 15:52:41 +0200

    erste Demo, unendlicher Boden bzw. Hintergrund

---
+++ Diplomarbeit/Assets/Scripts/CameraRunner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRunner : MonoBehaviour {
+	public Transform player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (player.position.x +3, 0, -10);
+	}
+}
---
+++ Diplomarbeit/Assets/Scripts/SceneryMover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneryMover : MonoBehaviour {
+
+	public float amount = 109.2f; // Width 2x of Scenery Prefab
+
+	// Use this for initialization
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player") 
+		{
+			Vector3 pos = gameObject.transform.position;
+			pos.x += amount;
+			gameObject.transform.position = pos;
+		}
+	}
+}
