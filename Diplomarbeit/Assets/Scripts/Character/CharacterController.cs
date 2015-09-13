6e4df63c9d2f8eaba9856383c5bdb0d18c399aed
Roman Socovka <socovka@gmx.at>
2015-09-13 21:45:28 +0200

    Finished FatChar- Prefab (incl. Animation-Idle; Animation-Running; Bee-Animation; Character-Animator; Bee-Animator);

---
+++ Diplomarbeit/Assets/Scripts/Character/CharacterController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+    public float MaxSpeed = 10f;
+
+    bool facingRight = true;
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("Speed", Mathf.Abs(move));
+        Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.velocity = new Vector2(move * MaxSpeed, rigidBody.velocity.y);
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+	}
+}
