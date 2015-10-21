8d4e445a1a4024b864d013f72f78ac505b8d4558
Roman Socovka <socovka@gmx.at>
2015-10-21 23:18:12 +0200

    Fully implemented Fat Char shooting

--- Diplomarbeit/Assets/Scripts/Character/CharacterController.cs
+++ Diplomarbeit/Assets/Scripts/Character/CharacterController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
 
 public class CharacterController : MonoBehaviour {
     public float MaxSpeed = 1f;
@@ -9,14 +10,22 @@ public class CharacterController : MonoBehaviour {
 	public float GroundCheckRadius;
 	public LayerMask  WhatIsGround;
     public bool runActivated;
+    private List<GameObject> bullets = new List<GameObject>();
 
-	private bool grounded = false;
+    private bool grounded = false;
 	private bool JumpPressed = false;
 
     public static float DEFAULT_MOVEMENT_RIGHT = 1f;
 
+    private bool shoot = true;
+    public GameObject bullet;
+
+    public float speed = 150f;
+
 
     bool facingRight = true;
+
+
     Animator anim;
 	Rigidbody2D rigidBody;
 
@@ -69,6 +78,41 @@ public class CharacterController : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        
-	}
+        if (shoot)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Shoot();
+            }
+        }
+    }
+
+    public void Shoot()
+    {
+        if ((Input.mousePosition.x > Screen.width / 5 || Input.mousePosition.y > Screen.height / 2.5) && (Input.mousePosition.x > 60 || Input.mousePosition.y < Screen.height - 60))
+        {
+            Vector3 target = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+            target = Camera.main.ScreenToWorldPoint(target);
+            target.Set(target.x, target.y, gameObject.transform.position.z);
+
+            GameObject obj = bullets.FirstOrDefault(x => !x.activeInHierarchy);
+            if (obj == null)
+            {
+                obj = (GameObject)Instantiate(bullet);
+                bullets.Add(obj);
+            }
+
+            obj.SetActive(true);
+            obj.transform.position = new Vector3(transform.position.x,transform.position.y,0);
+            obj.transform.rotation = transform.rotation;
+            obj.GetComponent<Rigidbody2D>().velocity = (target - transform.position).normalized * speed;
+            shoot = false;
+            Invoke("CanShootAgain", 1f);
+        }
+    }
+
+    private void CanShootAgain()
+    {
+        shoot = true;
+    }
 }
